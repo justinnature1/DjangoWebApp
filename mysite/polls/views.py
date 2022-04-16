ff8760a9a78403af2333086a1ea275c49222a639
justinnature1 <allistinkeller@gmail.com>
2022-04-15 19:42:30 -0500

    Add HTML Template

--- mysite/polls/views.py
+++ mysite/polls/views.py
@@ -1,8 +1,14 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from django.template import loader
 
 def index(request, name=''):
 	if (name == ''):
 		name = 'World'
-	return HttpResponse('Hello %s!' % name)
+	template = loader.get_template('polls/index.html')
+	message = ('Hello %s!' % name)
+	context = {
+		'message': message
+	}
+	return HttpResponse(template.render(context, request))
 
